6f4c2054a6cdc2097f47c3df6e2b58a5571dfadd
jcc <jcc@I7-JCC>
2019-05-15 17:16:32 +0100

    nuevo comint

--- src/Git.java
+++ src/Git.java
@@ -1,6 +1,6 @@
 
 public class Git {
-// Corrección  - -- - Prueba colaborativa desde 
+//nuevo comentario 17:16
     public static void main(String[] args) {
         //esto es una prueba
         
